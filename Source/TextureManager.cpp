b8e0aa5e8b869bd15e50b009bb9935054296ab22
Nadpher <Nadpher@gmail.com>
2022-03-21 19:26:12 +0100

    refactored texture manager

--- Source/TextureManager.cpp
+++ Source/TextureManager.cpp
@@ -6,4 +6,19 @@ namespace nadpher
 {
 	std::map<std::string, std::shared_ptr<sf::Texture>> TextureManager::textures_ = {};
 
+	std::shared_ptr<sf::Texture>& TextureManager::get(const std::string& path)
+	{
+		if (!textures_.count(path))
+		{
+			sf::Texture texture;
+			if (!texture.loadFromFile(path))
+			{
+				spdlog::error("Couldn't load texture {}", path);
+			}
+
+			textures_.insert({ path, std::make_shared<sf::Texture>(texture) });
+		}
+
+		return textures_[path];
+	}
 }
--- Source/TextureManager.h
+++ Source/TextureManager.h
@@ -13,21 +13,7 @@ namespace nadpher
 	class TextureManager
 	{
 	public:
-		static std::shared_ptr<sf::Texture>& get(const std::string& path)
-		{
-			if (!textures_.count(path))
-			{
-				sf::Texture texture;
-				if (!texture.loadFromFile(path))
-				{
-					spdlog::error("Couldn't load texture {}", path);
-				}
-
-				textures_.insert({ path, std::make_shared<sf::Texture>(texture) });
-			}
-
-			return textures_[path];
-		}
+		std::shared_ptr<sf::Texture>& get(const std::string& path);
 
 	private:
 		static std::map<std::string, std::shared_ptr<sf::Texture>> textures_;
